7c393cdab1a94578e8a4fa4e8f00787c3daa39da
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2015-12-19 15:06:38 +0530

    Update gruntfile, dependencies and .gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,34 @@
+var concat_tasks = {
+	core: {
+		src: ['src/app/app.module.js', 'src/app/app.route.js'],
+		dest: 'public/js/app.js'
+	},
+	controllers: {
+		src: ['src/app/app.controllers.js', 'src/app/components/**/*Controller.js'],
+		dest: 'public/js/controllers.js'
+	},
+	services: {
+		src: ['src/app/app.services.js', 'src/app/components/**/*Service.js'],
+		dest: 'public/js/services.js'
+	}
+};
+
+var copy_tasks = {
+	views: {
+		files: [
+			{expand: true, cwd:'src/app/', src: ['**/*.html'], dest: 'public/'}
+		]
+	}
+};
+
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+		concat: concat_tasks,
+		copy: copy_tasks
 	});
 
-	grunt.registerTask('default', []);
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.registerTask('default', ['concat', 'copy']);
 };
